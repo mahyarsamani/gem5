2549cd3cb124c5c05f3bc1ed155682cba4b9b426
Mahyar Samani <msamani@ucdavis.edu>
2025-01-10 02:25:17 +0000

    Adding an aggregate stat for read/write usefulness

--- src/mem/ruby/structures/CacheMemory.hh
+++ src/mem/ruby/structures/CacheMemory.hh
@@ -268,6 +268,9 @@ class CacheMemory : public SimObject
 
         statistics::Vector m_accessModeType;
 
+        statistics::Histogram readUsefulBytes;
+        statistics::Histogram writeUsefulBytes;
+
         statistics::Histogram intReadUsefulBytes;
         statistics::Histogram intWriteUsefulBytes;
 
